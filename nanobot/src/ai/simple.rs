ba30e71abd965de4145e6c7db4bf6c6e91910c5c
Seiichi KONDO <seikichi@kmc.gr.jp>
2018-07-21 14:11:46 +0900

    Update simple AI

--- nanobot/src/ai/simple.rs
+++ nanobot/src/ai/simple.rs
@@ -15,3 +15,27 @@ impl AI for SimpleAI {
         vec![]
     }
 }
+
+#[test]
+fn generate_commands_for_empty_3x3() {
+    let mut bytes: &[u8] = &[3, 0b00000000, 0b00000000, 0b00000000, 0b00000000];
+    let model = Model::new(&mut bytes).unwrap();
+
+    let ai = SimpleAI::new();
+    let commands = ai.generate(&model);
+    let expected = vec![Command::Halt];
+
+    assert_eq!(expected, commands);
+}
+
+#[test]
+fn generate_commands_for_non_empty_3x3() {
+    let mut bytes: &[u8] = &[3, 0b00000000, 0b00000000, 0b00000000, 0b00000000];
+    let model = Model::new(&mut bytes).unwrap();
+
+    let ai = SimpleAI::new();
+    let commands = ai.generate(&model);
+    let expected = vec![Command::Fill(NCD::new(1, 1, 1)), Command::Halt];
+
+    assert_eq!(expected, commands);
+}
