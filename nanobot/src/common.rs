25c301d3f1a32e17da02d185480d7a7227d2cb1b
MORI Shingo <cos@kmc.gr.jp>
2018-07-21 13:02:03 +0900

    add CD trait & new

--- nanobot/src/common.rs
+++ nanobot/src/common.rs
@@ -68,6 +68,21 @@ fn command_encode_test() {
     assert_eq!(flip[0], 0b11111101);
 }
 
+pub trait CD {
+    fn x(&self) -> i32;
+    fn y(&self) -> i32;
+    fn z(&self) -> i32;
+    fn manhattan_length(&self) -> i32 {
+        self.x().abs() + self.y().abs() + self.z().abs()
+    }
+    fn chessboard_length(&self) -> i32 {
+        vec![self.x().abs(), self.y().abs(), self.z().abs()]
+            .into_iter()
+            .max()
+            .unwrap()
+    }
+}
+
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Debug)]
 pub struct NCD {
     x: i32,
@@ -76,14 +91,30 @@ pub struct NCD {
 }
 
 impl NCD {
+    fn new(x: i32, y: i32, z: i32) -> Self {
+        let ncd = NCD { x, y, z };
+        assert!(ncd.manhattan_length() <= 2 && ncd.chessboard_length() == 1);
+        ncd
+    }
     fn encode(&self) -> u8 {
         ((self.x + 1) * 9 + (self.y + 1) * 3 + (self.z + 1)) as u8
+    }
+}
+impl CD for NCD {
+    fn x(&self) -> i32 {
+        self.x
+    }
+    fn y(&self) -> i32 {
+        self.y
+    }
+    fn z(&self) -> i32 {
+        self.z
     }
 }
 
 #[test]
 fn ncd_encode_test() {
-    let ncd = NCD { x: 1, y: 0, z: 0 };
+    let ncd = NCD::new(1, 0, 0);
     assert_eq!(ncd.encode(), 18 + 3 + 1);
 }
 
@@ -94,6 +125,15 @@ pub struct SLCD {
     z: i32,
 }
 impl SLCD {
+    fn new(x: i32, y: i32, z: i32) -> Self {
+        let slcd = SLCD { x, y, z };
+        assert!(
+            slcd.manhattan_length() <= 5
+                && slcd.chessboard_length() > 0
+                && slcd.manhattan_length() == slcd.chessboard_length()
+        );
+        slcd
+    }
     fn encode(&self) -> (u8, u8) {
         let ret = if self.x != 0 {
             (0b01, self.x + 5)
@@ -105,9 +145,20 @@ impl SLCD {
         (ret.0, ret.1 as u8)
     }
 }
+impl CD for SLCD {
+    fn x(&self) -> i32 {
+        self.x
+    }
+    fn y(&self) -> i32 {
+        self.y
+    }
+    fn z(&self) -> i32 {
+        self.z
+    }
+}
 #[test]
 fn slcd_encode_test() {
-    let slcd = SLCD { x: -3, y: 0, z: 0 };
+    let slcd = SLCD::new(-3, 0, 0);
     let enc = slcd.encode();
     assert_eq!(enc.0, 1);
     assert_eq!(enc.1, 2);
@@ -120,6 +171,15 @@ pub struct LLCD {
     z: i32,
 }
 impl LLCD {
+    fn new(x: i32, y: i32, z: i32) -> Self {
+        let llcd = LLCD { x, y, z };
+        assert!(
+            llcd.manhattan_length() <= 15
+                && llcd.chessboard_length() > 0
+                && llcd.manhattan_length() == llcd.chessboard_length()
+        );
+        llcd
+    }
     fn encode(&self) -> (u8, u8) {
         let ret = if self.x != 0 {
             (0b01, self.x + 15)
@@ -131,9 +191,20 @@ impl LLCD {
         (ret.0, ret.1 as u8)
     }
 }
+impl CD for LLCD {
+    fn x(&self) -> i32 {
+        self.x
+    }
+    fn y(&self) -> i32 {
+        self.y
+    }
+    fn z(&self) -> i32 {
+        self.z
+    }
+}
 #[test]
 fn llcd_encode_test() {
-    let llcd = LLCD { x: 0, y: 10, z: 0 };
+    let llcd = LLCD::new(0, 10, 0);
     let enc = llcd.encode();
     assert_eq!(enc.0, 2);
     assert_eq!(enc.1, 25);
