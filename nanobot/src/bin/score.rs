39543ce24428300ae8764b1ba923098d44b679e3
MORI Shingo <cos@kmc.gr.jp>
2018-07-22 17:41:50 +0900

    add bin/score

---
+++ nanobot/src/bin/score.rs
@@ -0,0 +1,99 @@
+extern crate getopts;
+extern crate nanobot_lib;
+
+// mod common;
+// mod model;
+// mod state;
+use getopts::Options;
+use nanobot_lib::common::read_trace_file;
+use nanobot_lib::model::Model;
+use nanobot_lib::state::State;
+
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("", "source", "set source model", "FILE");
+    opts.optopt("", "target", "set target model", "FILE");
+    opts.reqopt("", "trace", "set trace", "FILE");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            print_usage(&program, opts);
+            panic!(f.to_string())
+        }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    if !matches.opt_present("source") && !matches.opt_present("target") {
+        print_usage(&program, opts);
+        panic!("source or target file should be selected");
+    }
+
+    // Load
+    let trace = read_trace_file(Path::new(&matches.opt_str("trace").unwrap())).unwrap();
+    let mut r = 0;
+    let source_model = if matches.opt_present("source") {
+        let f = File::open(Path::new(&matches.opt_str("source").unwrap())).expect("file not found");
+        let mut f = BufReader::new(f);
+        let model = Model::new(&mut f).expect("failed to open model");
+        r = model.matrix.len();
+        Some(model)
+    } else {
+        None
+    };
+    let target_model = if matches.opt_present("target") {
+        let f = File::open(Path::new(&matches.opt_str("target").unwrap())).expect("file not found");
+        let mut f = BufReader::new(f);
+        let model = Model::new(&mut f).expect("failed to open model");
+        r = model.matrix.len();
+        Some(model)
+    } else {
+        None
+    };
+    let source_model = match source_model {
+        Some(model) => model,
+        None => Model::initial(r),
+    };
+    let target_model = match target_model {
+        Some(model) => model,
+        None => Model::initial(r),
+    };
+    if source_model.matrix.len() != r || target_model.matrix.len() != r {
+        panic!("source_model and target_model size are not same")
+    }
+    let mut state = State::initial_with_model(&source_model);
+
+    // Simulate
+    let mut offset = 0;
+    while offset < trace.len() {
+        let bot_cnt = state.get_bot_count();
+        match state.update_time_step(&trace[offset..offset + bot_cnt]) {
+            Ok(_) => {}
+            Err(err) => {
+                panic!(err.to_string());
+            }
+        }
+        offset += bot_cnt;
+    }
+
+    println!("Success:: ");
+    println!("Time:      ?");
+    println!("Commands:  {}", trace.len());
+    println!("Energy:    {}", state.get_energy());
+    println!("ClockTime: ?ms");
+}
---
+++ nanobot/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod ai;
+pub mod common;
+pub mod model;
+pub mod state;
+pub mod union_find;
