3a91e3107ea19b0bede86ca083bd62af77552a61
Seiichi KONDO <seikichi@kmc.gr.jp>
2018-07-22 19:32:33 +0900

    Add cargo run docs

--- bin/generate-all.sh
+++ bin/generate-all.sh
@@ -8,13 +8,13 @@ OUTPUT=$2
 cd nanobot
 
 for i in {001..186}; do
-    cargo run --release assemble ${PROBLEM}/FA${i}_tgt.mdl ${OUTPUT}/FA${i}.nbt;
+    cargo run --bin nanobot --release assemble ${PROBLEM}/FA${i}_tgt.mdl ${OUTPUT}/FA${i}.nbt;
 done
 
 for i in {001..186}; do
-    cargo run --release disassemble ${PROBLEM}/FD${i}_src.mdl ${OUTPUT}/FD${i}.nbt;
+    cargo run --bin nanobot --release disassemble ${PROBLEM}/FD${i}_src.mdl ${OUTPUT}/FD${i}.nbt;
 done
 
 for i in {001..115}; do
-    cargo run --release reassemble ${PROBLEM}/FR${i}_{src,tgt}.mdl ${OUTPUT}/FR${i}.nbt;
+    cargo run --bin nanobot --release reassemble ${PROBLEM}/FR${i}_{src,tgt}.mdl ${OUTPUT}/FR${i}.nbt;
 done
---
+++ nanobot/README.md
@@ -0,0 +1,13 @@
+# How to use
+
+## nanobot
+
+```sh
+$ cargo run --release --bin nanobot assemble model.mdl trace.nbt
+```
+
+## score
+
+```sh
+$ cargo run --release --bin score -- --trace dfltTracesF\FR115.nbt --source problemsF\FR115_tgt.mdl --target problemsF\FR115_tgt.mdl
+```
